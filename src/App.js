87bb57c4ed2cc46d6ec5ce1b842ed68b3076bf6f
Christina Loiacono <loiaconochristina@gmail.com>
2023-02-12 13:56:06 -0500

    fetched user data from API using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import SearchBar from './components/SearchBar/SearchBar';
 import Users from './components/Users/Users';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  // hooks
+  const [users, setUsers] = useState([]);
+
   // TODO: Fetch data here
+  useEffect(() => {
+    axios.get('https://users-app-backend.onrender.com/users')
+      .then((response) => {
+        setUsers(response.data)
+      })
+      .catch((error) => console.log(error))
+  }, []);
+
+  // console.log("users:", users)
 
   return (
     <div className="App">
