b369881034425e731b3ab38383236471e22bc3e5
JuHaoChen1997 <juhaochen97@gmail.com>
2023-02-12 14:35:11 -0500

    implemented all todos

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,6 @@
-import './User.css';
+import "./User.css";
 
-const User = ({ user }) => {
+const User = ({ user, expand, handleExpand }) => {
   const { about, age, company, country, name, photo } = user;
 
   return (
@@ -15,13 +15,17 @@ const User = ({ user }) => {
           <li>Country: {country}</li>
           <li>Company: {company}</li>
         </ul>
-        <div className="User__about">
-          <h3>About {name.split(' ')[0]}:</h3>
-          <p>{about}</p>
-        </div>
+        {expand.includes(user.id) && (
+          <div className="User__about">
+            <h3>About {name.split(" ")[0]}:</h3>
+            <p>{about}</p>
+          </div>
+        )}
       </div>
       <div className="User__controls">
-        <button>click me</button>
+        <button onClick={handleExpand}>
+          {expand.includes(user.id) ? "Show Less" : "Show More"}
+        </button>
       </div>
     </section>
   );
